725bea06d88220f89116120d8102107ad1d40ed7
Andrew Thomas <andrew@andrewrthomas.com>
2024-03-31 12:05:58 -0700

    starting with uniform partition

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use realfft::{ComplexToReal, RealToComplex};
+use std::sync::Arc;
+
+pub struct UPConv {
+    fft: Arc<dyn RealToComplex<f32>>,
+    ifft: Arc<dyn ComplexToReal<f32>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+impl UPConv {
+    pub fn new() -> Self {}
 }
