2aafb8ad6605f140aafa7ca017c0b7762339010b
Andrew Thomas <andrew@andrewrthomas.com>
2024-04-01 19:13:26 -0700

    ok something has been made, dont ask me what it is though

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,20 @@
 use num::{Float, Zero};
 use realfft::RealFftPlanner;
 use realfft::{num_complex::Complex, ComplexToReal, FftNum, RealToComplex};
-use std::collections::VecDeque;
 use std::sync::Arc;
 
-pub struct UPConv<T: FftNum> {
+pub struct UPConv<T: Float + FftNum, const N: usize> {
     fft: Arc<dyn RealToComplex<T>>,
     ifft: Arc<dyn ComplexToReal<T>>,
     input_buffer: Vec<T>,
     output_buffer: Vec<T>,
-    new_block_buffer: Vec<Complex<T>>,
     block_size: usize,
     filter: Vec<Vec<Complex<T>>>,
-    fdl: VecDeque<Vec<Complex<T>>>,
+    fdl: Vec<Vec<Complex<T>>>,
+    comp_buff: Vec<Complex<T>>,
 }
 
-impl<T: Float + FftNum> UPConv<T> {
+impl<T: Float + FftNum, const N: usize> UPConv<T, N> {
     pub fn new(block_size: usize, max_filter_size: usize) -> Self {
         let mut planner = RealFftPlanner::<T>::new();
         let fft = planner.plan_fft_forward(block_size * 2);
@@ -23,13 +22,12 @@ impl<T: Float + FftNum> UPConv<T> {
 
         let input_buffer = fft.make_input_vec();
         let output_buffer = ifft.make_output_vec();
+        let comp_buff = ifft.make_input_vec();
 
         let p = max_filter_size.div_ceil(block_size);
         let filter = vec![Vec::with_capacity(block_size + 1); p];
 
-        let fdl = VecDeque::from(vec![Vec::with_capacity(block_size + 1); p]);
-
-        let new_block_buffer = Vec::with_capacity(block_size + 1);
+        let fdl = vec![Vec::with_capacity(block_size + 1); p];
 
         Self {
             fft,
@@ -39,7 +37,7 @@ impl<T: Float + FftNum> UPConv<T> {
             output_buffer,
             filter,
             fdl,
-            new_block_buffer,
+            comp_buff,
         }
     }
 
@@ -65,16 +63,32 @@ impl<T: Float + FftNum> UPConv<T> {
         }
     }
 
-    pub fn process_block(&mut self, block: &mut [T]) {
-        self.input_buffer.rotate_left(self.block_size);
+    pub fn process_block(&mut self, block: &mut [T]) -> &[T] {
+        self.input_buffer.copy_within(self.block_size.., 0);
         self.input_buffer[self.block_size..].copy_from_slice(block);
 
-        self.new_block_buffer = self.fdl.pop_front().unwrap();
+        let fdl_len = self.fdl.len();
+
+        self.fdl.rotate_left(fdl_len - 1);
 
         self.fft
-            .process_with_scratch(block, &mut self.new_block_buffer, &mut [])
+            .process_with_scratch(&mut self.input_buffer, &mut self.fdl[0], &mut [])
             .unwrap();
 
-        self.fdl.push_back(self.new_block_buffer);
+        self.multiply_blocks();
+
+        self.ifft
+            .process_with_scratch(&mut self.comp_buff, &mut self.output_buffer, &mut [])
+            .unwrap();
+
+        &self.output_buffer[self.block_size..]
+    }
+
+    fn multiply_blocks(&mut self) {
+        for (filter_block, fdl_block) in self.filter.iter().zip(&self.fdl) {
+            for i in 0..self.block_size + 1 {
+                self.comp_buff[i] = self.comp_buff[i] + (filter_block[i] * fdl_block[i]);
+            }
+        }
     }
 }
